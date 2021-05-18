04338a506d751bc11d13aae86001d342e53cb1bd
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-05-18 20:42:04 -0300

    Adicionado layout da barra inferior de navegação

--- src/components/BottomNav/index.tsx
+++ src/components/BottomNav/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Link from "next/link";
 
+import styles from "./styles.module.scss";
+
 export function BottomNav() {
     return (
-        <nav>
+        <nav className={styles.bottomNavContainer}>
             <Link href="#">Meus favoritos</Link>
         </nav>
     )
