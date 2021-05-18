b6c91962b5bca913ba1aa769fa3aa0ca357b396a
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-05-18 20:42:48 -0300

    Adicionado barra de navegação inferior no template base do projeto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import 'styles/globals.scss'
 
+import { BottomNav } from "components/BottomNav";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <BottomNav />
+    </div>
+  )
 }
 
 export default MyApp
