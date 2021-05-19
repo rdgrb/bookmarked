292d9bb0c50be7398d603bdb256926b87f73037c
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-05-18 22:53:38 -0300

    Adicionado Provider do BookContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import 'styles/globals.scss'
 
 import { BottomNav } from "components/BottomNav";
+import { BookProvider } from 'src/contexts/BookContext';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <Component {...pageProps} />
-      <BottomNav />
+      <BookProvider>
+        <Component {...pageProps} />
+        <BottomNav />
+      </BookProvider>
     </div>
   )
 }
