ce7000efe360af04d85afb12f995b80b1eab7976
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-05-19 22:50:51 -0300

    Adicionado feedback de carregamento de página com o nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,23 @@
 import 'styles/globals.scss'
 
+import Router from "next/router";
+
 import { BottomNav } from "components/BottomNav";
 import { BookProvider } from 'src/contexts/BookContext';
+
+import NProgress from "nprogress";
+
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+});
+
+Router.events.on("routeChangeComplete", () => {
+  NProgress.done();
+});
+
+Router.events.on("routeChangeError", () => {
+  NProgress.done();
+});
 
 function MyApp({ Component, pageProps }) {
   return (
