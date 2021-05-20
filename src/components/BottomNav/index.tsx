05f1851a7dcd1a31df036a6dde433d6cdd9ad6bd
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-05-20 16:57:37 -0300

    Adicionado componente de livros favoritos na barra inferior

--- src/components/BottomNav/index.tsx
+++ src/components/BottomNav/index.tsx
@@ -1,12 +1,25 @@
+import { BookFavorites } from 'components/BookFavorites';
 import React from 'react'
-import Link from "next/link";
+import useCollapse from 'react-collapsed';
 
 import styles from "./styles.module.scss";
 
 export function BottomNav() {
+    const { getCollapseProps, getToggleProps, isExpanded } = useCollapse({
+        duration: 600,
+    });
+
     return (
         <nav className={styles.bottomNavContainer}>
-            <Link href="#">Meus favoritos</Link>
+            <button {...getToggleProps()}>
+                { isExpanded ? "Fechar" : "Meus favoritos" }
+            </button>
+
+            <section className={styles.favContainer}
+                {...getCollapseProps()}
+            >
+                <BookFavorites />
+            </section>
         </nav>
     )
 }
