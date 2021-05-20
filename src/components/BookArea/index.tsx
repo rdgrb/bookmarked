d2a2d6413cbe8b412ba76f16362c8ef43b59f691
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-05-20 16:59:32 -0300

    Adicionado uso dos métodos de gerenciamento de livros

--- src/components/BookArea/index.tsx
+++ src/components/BookArea/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Router from "next/router";
 
 import { BookImage } from 'components/BookImage';
@@ -8,11 +8,45 @@ import styles from "./styles.module.scss";
 import { Button } from 'components/Button';
 
 export default function BookArea() {
-    const { showBookCover, selectedBook } = useContext(BookContext);
+    const { 
+        showBookCover, 
+        selectedBook, 
+        setFavs, 
+        favoritesBook, 
+        bookRetrieved,
+        removeFavoriteBook,
+    } = useContext(BookContext);
+
+    const [favoriteBook, setFavoriteBook] = useState<boolean>(false);
 
     function redirectToBookPage() {
         Router.push(`/book/${ selectedBook.id }`)
     }
+
+    function addBookToFavorites() {
+        setFavs(selectedBook);
+        setFavoriteBook(true);
+    }
+
+    function removeBookFromFavorites() {
+        removeFavoriteBook(selectedBook);
+        setFavoriteBook(false);
+    }
+
+    useEffect(() => {
+        let favorite: boolean = false;
+        if(selectedBook) {
+            for (let i = 0; i < favoritesBook.length; i++) {
+                if (favoritesBook[i].id === selectedBook.id) {
+                    console.log(favoritesBook[i].id, selectedBook.id)
+                    favorite = true;
+                    break;
+                }
+            }
+        }
+
+        setFavoriteBook(favorite);
+    }, [selectedBook])
 
     return (
         <div className={styles.bookAreaContainer}>
@@ -32,21 +66,33 @@ export default function BookArea() {
                         { selectedBook.description }
                     </p>
 
-                    {/* <Link href={`/book/${ selectedBook.id }`}>
-                        <button className="primaryButton">Ver mais detalhes</button>
-                    </Link> */}
-
-                    <Button 
-                        primaryAction={redirectToBookPage}
-                    />
+                    {favoriteBook ? (
+                        <Button cancelButton
+                            primaryAction={redirectToBookPage}
+                            secondaryAction={removeBookFromFavorites}
+                        />
+                    ) : (
+                        <Button 
+                            primaryAction={redirectToBookPage}
+                            secondaryAction={addBookToFavorites}
+                        />
+                    )}
                 </div>
             )}
 
             {showBookCover && (
                 <div className={styles.bookCoverContainer}>
                     <BookImage uri={selectedBook.cover} />
-    
-                    <button className="primaryButton">Adicionar aos favoritos</button>
+
+                    {favoriteBook ? (
+                        <button className="btnCancel" onClick={removeBookFromFavorites}>
+                            Remover dos favoritos
+                        </button>
+                    ) : (
+                        <button className="btnPrimary" onClick={addBookToFavorites}>
+                            Adicionar aos favoritos
+                        </button>
+                    )}
                 </div>
             )}
         </div>
